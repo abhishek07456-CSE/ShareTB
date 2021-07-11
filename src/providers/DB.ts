ca145b65fc5f4e4b701aadee4ef1fe4a8a73a740
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-11 18:59:49 +0530

    UPDATE

--- src/providers/DB.ts
+++ src/providers/DB.ts
@@ -2,19 +2,19 @@
 import mongoose from 'mongoose';
 import Local from './Local';
 class DB {
-     public static db : any;
-     public init() : any{
+     public static db: any;
+     public init(): any {
           const url = Local.config().DbUrl;
           const dbName = Local.config().DbName;
-          const options = { useNewUrlParser: true };
+          const options = { useNewUrlParser: true, seUnifiedTopology: true };
           console.log(`${url}/${dbName}`);
-          mongoose.connect(`${url}/${dbName}`,options ,(err:any) =>{
-              if(err){
-                   console.log(`ERROR WHILE CONNECTING TO DB : ${err}`)
-              }else{
-                   DB.db = mongoose.connection;
-                   console.log("connection success");
-              }
+          mongoose.connect(`${url}/${dbName}`, options, (err: any) => {
+               if (err) {
+                    console.log(`ERROR WHILE CONNECTING TO DB : ${err}`)
+               } else {
+                    DB.db = mongoose.connection;
+                    console.log("connection success");
+               }
           })
      }
 }
