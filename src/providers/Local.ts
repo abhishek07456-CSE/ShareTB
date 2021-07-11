61a2613e90558c9a0fd3773091db30da46f9370c
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-11 20:26:49 +0530

    db name not found in env

--- src/providers/Local.ts
+++ src/providers/Local.ts
@@ -18,7 +18,7 @@ class Locals {
         const maxUploadLimit = process.env.APP_MAX_UPLOAD_LIMIT || '50mb';
 		const maxParameterLimit = process.env.APP_MAX_PARAMETER_LIMIT || '50mb';
         const DbUrl  = process.env.DB_URL  || 'mongodb://localhost:27017';
-        const DbName = process.env.DB_NAME || 'ShareTube';
+        const DbName = process.env.DB_NAME || 'sharetube';
         return {
             port,
             url,
