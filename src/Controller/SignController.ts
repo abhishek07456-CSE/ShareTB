05941ad9a49fb75d8b6d7562587395f9ba111ae4
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-17 03:18:28 +0530

    update

--- src/Controller/SignController.ts
+++ src/Controller/SignController.ts
@@ -1,7 +1,7 @@
 import SignupModel from "../Models/SignupModel";
 class SignController {
       public static  registration_Controller = (req : any ,res : any) : any => {
-            let data = new SignupModel().saveSignupDetails(req.body);
+            const data = new SignupModel().saveSignupDetails(req.body);
             res.send(data);
       }
 }
--- src/providers/DbsAction.ts
+++ src/providers/DbsAction.ts
@@ -1,11 +1,13 @@
 import DB from "./DB";
 export default class DbsAction extends DB{
+    private static response : any;
     public static operation = (callable) => (schema) => (data) => callable(schema,data);
     public static insert = (schema: any, data: any) => {
         DB.db.collection(schema).insert(data, (err: any, result: any): any => {
         if (err)
-            return { err: err.message };
-        return result;
-     });
+          DbsAction.response = err;
+          DbsAction.response = result;
+        });
+        return DbsAction.response;
    }
 }
