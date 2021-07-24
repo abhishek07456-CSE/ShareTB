b895c5bf0daaa1c28fb55f56b7c0c6b61f30b0b0
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-25 02:29:10 +0530

    update for crete at delated at

--- src/Schema/UserSchema.ts
+++ src/Schema/UserSchema.ts
@@ -3,36 +3,41 @@ import mongoose from "mongoose";
 import validator from 'validator';
 import { beforeSaveHook } from "./BaseMiddleware";
 const schema = mongoose.Schema;
-var UserSchema: any = new schema<IUserModel>({
-    email: {
-        type: schema.Types.String,
-        required: true,
-        unique: true,
-        validate: (value) => {
-            return validator.isEmail(value)
+var UserSchema: any = new schema<IUserModel>(
+    {
+        email: {
+            type: schema.Types.String,
+            required: true,
+            unique: true,
+            validate: (value) => {
+                return validator.isEmail(value)
+            }
+        },
+        first_name: {
+            type: schema.Types.String,
+            required: true,
+        },
+        last_name: {
+            type: schema.Types.String,
+            required: true,
+        },
+        password: {
+            type: schema.Types.String,
+            required: true,
+            validate: (value) => {
+                return validator.isStrongPassword(value)
+            }
         }
     },
-    first_name: {
-        type: schema.Types.String,
-        required: true,
-    },
-    last_name: {
-        type: schema.Types.String,
-        required: true,
-    },
-    password: {
-        type: schema.Types.String,
-        required: true,
-        validate: (value) => {
-            return validator.isStrongPassword(value)
-        }
+    {
+        timestamps: true
     }
-});
+);
 
-UserSchema.pre('save', (next) => {
-    beforeSaveHook(this);
-    next();
-});
+// UserSchema.pre('save', (next) => {
+//     beforeSaveHook(this);
+//     next();
+// });
 const User = mongoose.model<IUserModel>('user', UserSchema);
 
 export default User;
