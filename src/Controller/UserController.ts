8fac15f6796d353d62df8a9f20a3f4202e5bb1a3
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-26 16:47:27 +0530

    update

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -1,18 +1,19 @@
 import LoginRequest from "../Schema/LoginRequest";
 import UserService from "../Service/UserService";
 class SignController {
-      public static  register = async(req : any ,res : any , next : any) => {
+      public static register = async (req: any, res: any, next: any) => {
             const data = await UserService.signup(req.body);
             res.json(data);
             next();
       }
-      public static  login = async (req : any , res : any , next : any) => {
-             res.json(res.data);
-             next();
+      public static login = async (req: any, res: any, next: any) => {
+            res.json(res.data);
+            next();
       }
 
-      public static getProfile = async (req : any , res : any , next : any) => {
-            return res.status(200).json(res.user);
+      public static getProfile = async (req: any, res: any, next: any) => {
+            res.status(200).json(res.user);
+            next();
       }
 }
 
--- src/Middleware/Authenticator.ts
+++ src/Middleware/Authenticator.ts
@@ -26,17 +26,18 @@ export class Authenticator {
     };
     public static refreshToken = async (request: any , response : any , next : any) => {
         let _user: any = new User();
-        _user = await _user.collection.findOne({ email: request.email });
+        _user = await _user.collection.findOne({ email: request.body.email });
+        console.log(_user);
         //to-do make IUserMode variable initialize and make global EXCEPTION
         if (_user==null){
              return next(Handler.throwError('User Not Found', Handler.NOT_FOUND));
-        }else if(!compareHash(request.password,_user.password)){
+        }else if(!compareHash(request.body.password,_user.password)){
              return next(Handler.throwError('Password Not Correct', Handler.NOT_ALLOWED));
         }
         console.log(_user);
         let token = await jwt.sign({id:_user._id,email:_user.email}, Local.config().TOKEN_SECRET, { expiresIn: Local.config().TOKEN_EXPIRE_TIME });
         _user['token'] = token;
         response.data = _user;
-        return _user;
+        next();
     }
 }
