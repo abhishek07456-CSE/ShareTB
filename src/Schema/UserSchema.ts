848b346214cf1b7a146b91649ae0add8b8672750
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-30 01:42:18 +0530

    update

--- src/Schema/UserSchema.ts
+++ src/Schema/UserSchema.ts
@@ -8,6 +8,8 @@ let definition: any = new schema<IUserModel>(
         email: {
             type: schema.Types.String,
             required: true,
+            trim: true,
+            lowercase: true,
             unique: true,
             validate: (value) => {
                 return validator.isEmail(value)
@@ -16,10 +18,12 @@ let definition: any = new schema<IUserModel>(
         first_name: {
             type: schema.Types.String,
             required: true,
+            trim: true,
         },
         last_name: {
             type: schema.Types.String,
             required: true,
+            trim: true,
         },
         password: {
             type: schema.Types.String,
@@ -30,7 +34,17 @@ let definition: any = new schema<IUserModel>(
             set: (value: string): string => {
                 return encrypt(value);
             }
-        }
+        },
+        gender: {
+            type: schema.Types.String,
+            enum: ['0', 'M', 'F'],
+            default: null
+        },
+        age: {
+            type: schema.Types.Number,
+            min: 0,
+            default: 0
+        },
     },
     {
         timestamps: true
@@ -41,6 +55,15 @@ let definition: any = new schema<IUserModel>(
 //     beforeSaveHook(this);
 //     next();
 // });
+//to-do : create enum file where all enum files exists
+definition.virtual('Gender').get((value) => {
+    const mapping = {
+        'O': 'Others',
+        'M': 'Male',
+        'F': 'Female',
+    }
+    return mapping[value];
+});
 const UserSchema = mongoose.model<IUserModel>('user', definition);
 
 export default UserSchema;
